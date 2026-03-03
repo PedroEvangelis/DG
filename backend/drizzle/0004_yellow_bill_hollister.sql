36a6f2af91e0e9f98276481047482c66b9c680e6
Pedro Evangelista <pedroevangelistabrasil@gmail.com>
2026-03-03 19:07:53 -0300

    Ajuste de migração do banco

--- backend/drizzle/0004_yellow_bill_hollister.sql
+++ backend/drizzle/0004_yellow_bill_hollister.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "user" ADD COLUMN "banned" boolean;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "banned" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "user" ADD COLUMN "banReason" text;--> statement-breakpoint
 ALTER TABLE "user" ADD COLUMN "banExpires" timestamp;
--- backend/drizzle/0007_brief_terrax.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" ALTER COLUMN "banned" SET DEFAULT false;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "banned" SET NOT NULL;
